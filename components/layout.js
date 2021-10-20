afd14c4bf6477dc941b21bbb29a16722dc432116
Matthew Parks <mattparksjr@gmail.com>
2021-10-20 19:30:57 -0400

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 const name = "Matthew Parks"
 
@@ -14,38 +15,30 @@ export default function Layout({ children }) {
                     <div class="hidden text-xs text-center lg:block">
                         <main>{children}</main>
                     </div> 
-                    <div class="text-xs text-center lg:hidden"
-                        ><main>{children}</main>
+                    <div class="text-xs text-center lg:hidden">
+                        <main>{children}</main>
                     </div>
                 </div> 
                 <div class="drawer-side">
                     <label for="my-drawer-2" class="drawer-overlay"></label> 
-                    <ul class="menu p-4 overflow-y-auto w-60 bg-base-100 text-base-content">
-                        <div class="flex flex-col">
-                            <div class="avatar flex justify-center">
-                                <div class="rounded-full w-16 h-16 justify-center">
-                                    <img class="object-fill w-full" src="http://daisyui.com/tailwind-css-component-profile-1@94w.png"/>
-                                </div>
-                            </div> 
-                            <div class="flex justify-center">
-                                <p>{name}</p>
+                    <div class="menu flex flex-col p-4 overflow-y-auto w-60 h-full bg-base-100 text-base-content">
+                        <div class="avatar flex justify-center">
+                            <div class="rounded-full w-16 h-16 justify-center">
+                                <img class="object-fill w-full" src="http://daisyui.com/tailwind-css-component-profile-1@94w.png"/>
                             </div>
+                        </div> 
+                        <div class="flex justify-center">
+                            <p>{name}</p>
                         </div>
                         <div class="divider"></div> 
-                        <li><a>Dashboard</a></li>
-                        <li>
-                            <a>Income</a>
-                        </li> 
-                        <li>
-                            <a>Expenses</a>
-                        </li>
-                        <li>
-                            <a>Assets</a>
-                        </li>
-                        <div class="absolute bottom-0 left-0">
-                            <p>Mr. Krabs V0.0.1</p>
-                        </div>
-                    </ul>
+                        <ul class="flex flex-col justify-right">
+                            <li><Link href="/"><a>Dashboard</a></Link></li>
+                            <li><Link href="/income"><a>Income</a></Link></li>
+                            <li><Link href="/expenses"><a>Expenses</a></Link></li>
+                            <li><Link href="/assets"><a>Assets</a></Link></li>
+                        </ul>
+                            <p class="text-gray-50">Mr. Krabs V0.0.1</p>
+                    </div>
                 </div>
             </div>
         </div>
