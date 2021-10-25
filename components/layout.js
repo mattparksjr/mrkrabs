8c624cbeee0b728101de54345b2b4247e19d562e
Matthew Parks <mattparksjr@gmail.com>
2021-10-25 13:28:24 -0400

    Lots of layout work
    :)

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { DesktopComputerIcon,CubeIcon, UserIcon} from '@heroicons/react/solid'
+import { DesktopComputerIcon, CubeIcon, UserIcon, ArrowLeftIcon } from '@heroicons/react/solid'
+import Donut from "./donut.js"
 
 const name = "Matthew Parks"
 
@@ -8,43 +9,42 @@ export default function Layout({ children }) {
     return (
         <div>
             <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
-            <div class="relative min-h-screen md:flex">
-                {/*This is a comment*/}
-                <div class="bg-gray-800 text-gray-100">
-                    <UserIcon className="block w-8 h-8 text-white"/>
-                    <a href="#" class="block p-4 text-white font-bold"></a>
-                </div>
-
-                <div class="bg-blue-800 text-blue-100 w-52
-                 space-y-6 py-7 px-2 absolute inset-y-0
-                  left-0 transform -translate-x-full transition duration-200 ease-in-out">
+            <div class="bg-blue-800 text-blue-100 lg:w-1/6
+                  py-7 px-2 fixed inset-y-0
+                  left-0 transform  transition duration-200 ease-in-out">
+                <Link href="/">
+                    <a class="text-white flex items-center space-x-2 px-4">
+                        <CubeIcon className="w-8 h-8" />
+                        <span class="text-2xl font-extrabold">Mr. Krabs</span>
+                    </a>
+                </Link>
+                <div class="h-2 rounded bg-gray-300 w-full space-x-2 my-4"/>
+                <nav>
                     <Link href="/">
-                        <a class="text-white flex items-center space-x-2 px-4">
-                            <CubeIcon className="w-8 h-8"/>
-                            <span class="text-2xl font-extrabold">Mr. Krabs</span>
-                        </a>
+                        <a class="block py-2.5 px-4 rounded hover:bg-blue-900">Dashboard</a>
                     </Link>
-                    <nav>
-                        <Link href="/">
-                            <a class="block py-2.5 px-4 rounded hover:bg-blue-900">Dashboard</a>
-                        </Link>
-                        <Link href="/">
-                            <a class="block py-2.5 px-4 rounded hover:bg-blue-900">Assets</a>
-                        </Link>
-                        <Link href="/">
-                            <a class="block py-2.5 px-4 rounded hover:bg-blue-900">Income</a>
-                        </Link>
-                        <Link href="/">
-                            <a class="block py-2.5 px-4 rounded hover:bg-blue-900">Spending</a>
-                        </Link>
-                    </nav>
-                </div>
-
-                <div class="p-10 text-2xl font-bold flex-1">
-                    {children}
+                    <Link href="/">
+                        <a class="block py-2.5 px-4 rounded hover:bg-blue-900">Assets</a>
+                    </Link>
+                    <Link href="/">
+                        <a class="block py-2.5 px-4 rounded hover:bg-blue-900">Income</a>
+                    </Link>
+                    <Link href="/">
+                        <a class="block py-2.5 px-4 rounded hover:bg-blue-900">Spending</a>
+                    </Link>
+                </nav>
+                <div class="fixed left-0 bottom-0 w-full">
+                    <p class="absolute py-2.5 px-4 rounded bottom-0">Mr. Krabs v0.0.1</p>
+                    <ArrowLeftIcon className="md:invisible w-8 h-8"/>
                 </div>
             </div>
+
+            <div class="text-2xl font-bold flex w-5/6 float-right" >
+                {children}
+            </div>
         </div>
+
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,29 @@
 import Link from 'next/link'
+import Donut from '../components/donut'
 import Layout from '../components/layout'
 
 export default function Home() {
   return (
     <Layout>
       <main>
+        <div class="h-128 w-128">
+        <Donut/>
+        </div>
         <h1 className="title">
           Read{' '}
           <Link href="/income">
             <a>this page!</a>
           </Link>
         </h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consequat molestie enim, blandit maximus nisi finibus ut. Vivamus lacinia venenatis tempor. Phasellus eget aliquet eros. Nam finibus iaculis enim, sit amet imperdiet mi aliquam ac. Phasellus nec aliquam sapien, et lacinia lorem. In tristique velit et tempus porttitor. Praesent vel condimentum velit, sed porttitor ante. Curabitur sed aliquet massa, ullamcorper dictum urna. Integer luctus ipsum non nibh faucibus, non blandit nisi tincidunt. Proin ultricies lectus urna, sed pretium turpis egestas vitae. Morbi dapibus accumsan velit non dignissim. Morbi imperdiet vestibulum sapien, et iaculis mauris facilisis a. Fusce vitae molestie ligula. Integer nibh erat, aliquam consequat mattis vitae, mattis vitae tellus.
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+Sed elementum neque in tortor lobortis facilisis. Nunc aliquet, velit sit amet posuere gravida, urna ligula accumsan ante, nec tincidunt dolor felis vel nibh. Etiam interdum rhoncus felis vulputate pellentesque. Integer sapien metus, egestas at commodo eu, aliquet volutpat ligula. Maecenas pulvinar accumsan libero, at euismod nunc semper non. Cras aliquet ipsum ultrices lectus facilisis feugiat. Duis et est pulvinar, sagittis libero sed, sagittis neque. Morbi diam lectus, ultricies sed placerat eget, consequat id metus. Cras nec egestas lacus, nec auctor metus. Maecenas lorem ipsum, lobortis quis erat in, rhoncus consectetur est.
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+Donec quis ullamcorper dui. Pellentesque ac eros sollicitudin, pellentesque nunc sit amet, tristique sapien. Praesent eget justo felis. Quisque cursus sodales orci vitae dapibus. Duis cursus interdum nibh, ut iaculis dui vehicula ut. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis aliquet ante id ipsum sodales porttitor. Aliquam tempus erat sed dictum posuere. Pellentesque et bibendum odio. Sed accumsan est cursus lacus tempus volutpat. Aenean non varius ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam vel vestibulum orci, at sodales dui.
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+Nam molestie neque nunc, sed pellentesque turpis vehicula sed. Aenean suscipit, diam non aliquet ullamcorper, erat velit dictum felis, a lobortis nisl urna ut ligula. Morbi vel justo eu urna rhoncus tincidunt sit amet ut lorem. In fermentum varius ligula, quis pulvinar diam dictum ut. Fusce a magna ut risus vestibulum tempor ut a sem. Maecenas sed imperdiet eros. Aliquam hendrerit, est sit amet aliquet accumsan, sapien enim tincidunt lorem, vitae vestibulum tortor urna a dolor. Donec nec metus sed augue sagittis consectetur. Curabitur scelerisque turpis et nibh rutrum faucibus. Sed feugiat varius egestas.
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+Aliquam finibus posuere mi sed tempor. In aliquam magna risus, vitae ultricies justo consequat sed. Morbi aliquam pretium scelerisque. Sed semper, nulla ac tempor aliquet, ipsum est pretium massa, vitae volutpat felis ante at risus. Nam eget luctus metus. Fusce venenatis purus ut nibh consectetur accumsan. Phasellus vitae diam eu ligula egestas aliquam. Curabitur rutrum justo in ante consectetur tristique. Maecenas non tempor ex, eget aliquet tortor. Maecenas est massa, sagittis id laoreet vel, molestie vitae odio. Fusce elementum nulla ultrices libero condimentum, ac pretium erat tincidunt. Sed maximus convallis tortor nec faucibus. Nulla tristique tortor tortor, vitae placerat massa sagittis sit amet. Curabitur aliquet, lorem eget dictum fermentum, felis augue sagittis leo, nec semper mauris ante feugiat nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae tincidunt est, id pellentesque turpis.
       </main>
     </Layout>
   )
