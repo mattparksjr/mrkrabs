553dd44c750a8d72775532a39668ae532af09211
Matthew Parks <mattparksjr@gmail.com>
2021-10-25 13:31:09 -0400

    please shut the up

--- components/donut.js
+++ components/donut.js
@@ -1,36 +1,36 @@
 import React from 'react';
-import {Doughnut} from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
 
 const data = {
   labels: [
     'Red',
     'Green',
     'Yellow'
-],
-datasets: [{
-  data: [300, 50, 100],
-  backgroundColor: [
-  '#FF6384',
-  '#36A2EB',
-  '#FFCE56'
   ],
-  hoverBackgroundColor: [
-  '#FF6384',
-  '#36A2EB',
-  '#FFCE56'
-  ]
-}]
+  datasets: [{
+    data: [300, 50, 100],
+    backgroundColor: [
+      '#FF6384',
+      '#36A2EB',
+      '#FFCE56'
+    ],
+    hoverBackgroundColor: [
+      '#FF6384',
+      '#36A2EB',
+      '#FFCE56'
+    ]
+  }]
 };
 
-const Donut = () => 
-<div>
-  <h2>Doughnut Example</h2>
-  <Doughnut
-     data={data}
-     width={400}
-     height={400}
-  />
-</div>
-;
 
-export default Donut;
+export default function Donut() {
+  return (
+    <div>
+      <h2>Doughnut Example</h2>
+      <Doughnut
+        data={data}
+        width={400}
+        height={400}
+      />
+    </div>);
+}
